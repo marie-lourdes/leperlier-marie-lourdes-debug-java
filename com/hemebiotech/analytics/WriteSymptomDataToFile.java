d2fba36e41edbf9ce31ade925716539d598ec29c
Marie-Lourdes Leperlier <kpz1k3bb8mq6-student@anonymous-codio.com>
2023-05-18 17:39:22 +0000

    Class-WriteSymptomDataToFile.java: create method displayListSymptoms() to test the methode getSymptoms() of interface implemented by ReadSymptomsDataFromFile.java

--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -6,7 +6,18 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
+	public WriteSymptomDataToFile() {
+		
+	}
 	@Override
-	public void writeSymptoms(Map<String, Integer> symptoms) {		
-	}	
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		
+	}
+	public void displayListSymptoms() {
+	    symptomsData = new ReadSymptomDataFromFile("symptoms.txt");
+		listSymptoms= symptomsData.getSymptoms();
+		for( String symptom: listSymptoms) {
+			System.out.println(symptom);
+		}
+			
 }
