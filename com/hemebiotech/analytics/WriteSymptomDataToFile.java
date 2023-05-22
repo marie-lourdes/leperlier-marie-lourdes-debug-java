d13185e8b93e89b8fe8746639676d7b804743010
Marie-Lourdes Leperlier <kpz1k3bb8mq6-student@anonymous-codio.com>
2023-05-22 08:51:24 +0000

    WriteSymptomDataToFile.java: add the creation of object Iterator in bloc try

--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -18,10 +18,15 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
     // Override the  method writeSymptoms of ISymptomWriter interface with Iterator to iterate and  display key -symptom and value - number of occurrences of each symptom
 	@Override
 	public void writeSymptoms(Map<String, Integer> symptoms) {
-
-        // Check if file exist and if there are content on the map symptoms and possible error in instruction try and intercept error in a instruction catch and print explanation of error
+        // Check if file exist and if there are content on the map symptoms 
+		//and possible error in instruction try and intercept error in a instruction catch and print explanation of error
         if (filePath != null && symptoms != null) {
             try {
+                //Create object Iterator to iterate on each entry of the mapSymptomsOccurencesSorted and write each entry in file
+                Iterator<Map.Entry<String, Integer>> iteratorMap = symptoms.entrySet().iterator(); 
+                // @param entrySymptomAndNumberOfOccurrences - type Map.Entry<String,Iteger> as a consumer (parameter of the method forEachRemaining used by the expression lambada)
+		        // the consumer consume  each entry of the instance iterator
+                
                 // Create object writer with path of file and second argument to write without remove all previous content 
                 // the object writer is used by bufferedReader which write line by line on file indicated in the filePath           
                 FileWriter fileWriter = new FileWriter(filePath, true);	
@@ -32,10 +37,7 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
             }
 		}
         
-        //Create object Iterator to iterate on each entry of the mapSymptomsOccurencesSorted and write each entry in file
-        Iterator<Map.Entry<String, Integer>> iteratorMap = symptoms.entrySet().iterator(); 
-        // @param entrySymptomAndNumberOfOccurrences - type Map.Entry<String,Iteger> as a consumer (parameter of the method forEachRemaining used by the expression lambada)
-		// the consumer consume  each entry of the instance iterator
+        
         iteratorMap.forEachRemaining(
 			(entrySymptomAndNumberOfOccurrences)->  System.out.println(
                 "number of " + entrySymptomAndNumberOfOccurrences.getKey() + ": " + entrySymptomAndNumberOfOccurrences.getValue()
