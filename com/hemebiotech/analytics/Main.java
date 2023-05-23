21bd6c060fca88cfc8f06e7f4e710695ca0367d9
Marie-Lourdes Leperlier <kpz1k3bb8mq6-student@anonymous-codio.com>
2023-05-23 08:09:52 +0000

    refactor analytics counter

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -4,8 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 // WRITE YOUR CODE HERE
-/*public class Main {
-    	public static void main(String args[]) throws Exception {
+public class Main {
+    	/**public static void main(String args[]) throws Exception {
            ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
            ISymptomWriter writer = new WriteSymptomDataToFile();
            AnalyticsCounter counter = new AnalyticsCounter(reader,writer); 
@@ -13,5 +13,5 @@ import java.util.Map;
            Map<String,Integer> mapSymptomsOccurences = counter.countSymptoms(readListSymptoms); 
            Map<String,Integer> mapSymptomsOccurencesSorted=counter.sortSymptoms(mapSymptomsOccurences);
            counter.writeSymptoms(mapSymptomsOccurencesSorted);
-        }
-}*/
+        }*/
+}
