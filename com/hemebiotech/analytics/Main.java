d7e2aaf06dced932e498a891ccd9a86aad771b2f
Marie-Lourdes Leperlier <kpz1k3bb8mq6-student@anonymous-codio.com>
2023-05-23 08:41:47 +0000

    Main.java class Main, its function main() and instance reader instance writer, counter, call method of class AnalycsCounter from the instance counter

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 // WRITE YOUR CODE HERE
 public class Main {
-    	/**public static void main(String args[]) throws Exception {
+    	public static void main(String args[]) throws Exception {
            ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
            ISymptomWriter writer = new WriteSymptomDataToFile();
            AnalyticsCounter counter = new AnalyticsCounter(reader,writer); 
@@ -13,5 +13,5 @@ public class Main {
            Map<String,Integer> mapSymptomsOccurences = counter.countSymptoms(readListSymptoms); 
            Map<String,Integer> mapSymptomsOccurencesSorted=counter.sortSymptoms(mapSymptomsOccurences);
            counter.writeSymptoms(mapSymptomsOccurencesSorted);
-        }*/
+        }
 }
