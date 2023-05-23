d1f0b18d076f6222a9e44b0634cdfdbfd2ebfffb
Marie-Lourdes Leperlier <kpz1k3bb8mq6-student@anonymous-codio.com>
2023-05-23 06:42:56 +0000

    AnalyticsCounter.java: add return to the method getSymptoms()

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -4,3 +4,14 @@ import java.util.List;
 import java.util.Map;
 
 // WRITE YOUR CODE HERE
+public class main {
+    	public static void main(String args[]) throws Exception {
+           ISymptomReader reader=new ReadSymptomDataFromFile("symptoms.txt");
+           ISymptomWriter writer = new WriteSymptomDataToFile();
+           AnalyticsCounter counter = new AnalyticsCounter(reader,writer); 
+           readListSymptoms= counter.getSymptoms;
+          counter.countSymptoms(readListSymptoms);
+           Map<String,Integer>mapSymptomsOccurences= new HashMap<String,Integer>();
+             counter.ortSymptoms(mapSymptomsOccurences);
+        }
+}
