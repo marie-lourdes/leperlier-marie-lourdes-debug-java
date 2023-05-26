e834350a2762e87193a3b1156ccc347d6e1e0db6
Marie-Lourdes Leperlier <kpz1k3bb8mq6-student@anonymous-codio.com>
2023-05-26 19:14:05 +0000

    test style programmation on the file ReadSymptomDataToFile.java

--- com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -1,7 +1,7 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,41 +12,44 @@ import java.util.List;
 */
 public class ReadSymptomDataFromFile implements ISymptomReader {
   private String filepath;
-  // constructor assigns to the attribute "filepath" a full or partial path to file with symptom strings in it, one per line
+
+  /* constructor assigns to the attribute "filepath" a full or partial path to file 
+  with symptom strings in it, one per line */
   public ReadSymptomDataFromFile(String filepath) {
-	this.filepath = filepath;
-}
-	
-/**
- * @throws IOException         if there's a problem in reading the file symptoms.txt
- *
- * @return                     a List of all String Symptoms obtained from a data source symptom.txt with unic and duplicated symptoms
- *
- * @see                        #getSymptoms()
-*/
+    this.filepath = filepath;
+  }
+
+  /**
+   * @throws IOException    if there's a problem in reading the file symptoms.txt
+   *
+   * @return    a List of all String Symptoms obtained from a data source symptom.txt 
+   *     with unic and duplicated symptoms
+   *
+   * @see    #getSymptoms()
+   */
   @Override
   public List<String> getSymptoms() {
     // creation of List to stock the symptoms from the file symptoms.txt
-	List<String> result = new ArrayList<String>();
-		
-	if (filepath != null) {
-	  try {
-	    BufferedReader reader = new BufferedReader(new FileReader(filepath));
-		String line = reader.readLine();
-				
-		while (line != null) {
-		  result.add(line);
-		  line = reader.readLine();
-		}
+    List<String> result = new ArrayList<String>();
 
-		reader.close();
-        System.out.println("successfully read, get all list of symtoms from symptoms.txt and adding in the readListSymptoms");		        	     
-	  } catch (IOException e) {
-          System.err.println("reading symptoms to file symptoms.txt failed and adding a list of symptoms failed : "+ e.getMessage());
-		  e.printStackTrace();
-		}
-	}
-        	
-	return result;
+    if (filepath != null) {
+      try {
+        BufferedReader reader = new BufferedReader(new FileReader(filepath));
+        String line = reader.readLine();
+
+        while (line != null) {
+          result.add(line);
+          line = reader.readLine();
+        }
+
+        reader.close();
+        System.out.println("successfully read");
+      } catch (IOException e) {
+        System.err.println("reading symptoms to file symptoms.txt failed" 
+            + "and adding a list of symptoms failed : " + e.getMessage());
+        e.printStackTrace();
+      }
+    }   
+    return result;  
   }
 }
